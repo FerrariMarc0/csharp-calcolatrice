452231af64ab44738a6226ce7e04e0c28b502d0f
FerrariMarc0 <marco1010@live.it>
2023-09-12 17:13:24 +0200

    calculator

--- csharp-calcolatrice/Math.cs
+++ csharp-calcolatrice/Math.cs
@@ -6,15 +6,84 @@ using System.Threading.Tasks;
 
 namespace csharp_calcolatrice
 {
-    public class Math
+    public static class MathHelperCalculator
     {
-        public static class MathHelperCalculator
+        public static int Sum(int number1, int number2)
         {
-            public static int Sum(int a, int b)
-            {
-                return a + b;
-            }
+            return number1 + number2;
+        }
 
+        public static double Sum(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public static int Subtraction(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        public static double Subtraction(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        public static int Multiplication(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        public static double Multiplication(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+
+        public static int Absolute(int number)
+        {
+            return Math.Abs(number); //number >= 0 ? number : - number
+        }
+
+        public static double Absolute(double number)
+        {
+            return Math.Abs(number); //number >= 0 ? number : - number
+        }
+
+        public static int MinValue(int number1 , int number2)
+        {
+            return Math.Min(number1, number2);
+        }
+
+        public static double MinValue(double number1 , double number2)
+        {
+            return Math.Min(number1, number2);
+        }
+
+        public static int MaxValue(int number1 , int number2)
+        {
+            return Math.Max(number1, number2);
+        }
+
+        public static double MaxValue(double number1 , double number2)
+        {
+            return Math.Max(number1, number2);
+        }
+
+        //Il fatto di dover scrivere lo stesso metodo per tipi di parametro diversi, applica nella pratica il PRINCIPIO DI LISKOV
+        //poichè le istanze posso essere sostituite con altre istanze senza alterare il corretto funzionamento del programma.
+
+        public static double Pow(double number, int exponent)
+        {
+            if (number == 0)
+            {
+                if (exponent == 0)
+                    return 1;
+                else
+                    return 0;
+            }
+            else
+                return Math.Pow(number, exponent);
         }
     }
+        //In questo caso ci risulterebbero molto utili i Generics di C#, potendo eseguire le operazioni sia su numeri interi sia su numeri double.
+
 }
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            
+            {
+                int number1 = 3;
+                int number2 = 7;
+                double number3 = 5.1;
+                double number4 = 8.3;
+
+                int intSum = MathHelperCalculator.Sum(number1, number2);
+                double doubleSum = MathHelperCalculator.Sum(number3, number4);
+
+                int intDifference = MathHelperCalculator.Subtraction(number1, number2);
+                double doubleDifference = MathHelperCalculator.Subtraction(number3, number4);
+
+                int intMultiplier = MathHelperCalculator.Multiplication(number1, number2);
+                double doubleMultiplier = MathHelperCalculator.Multiplication(number3, number4);
+
+                int intAbsolutify = MathHelperCalculator.Absolute(-number2);
+                int doubleAbsolutify = MathHelperCalculator.Absolute((int)number4);
+
+                int intMin = MathHelperCalculator.MinValue(number1, number2);
+                double doubleMin = MathHelperCalculator.MinValue(number3, number4);
+
+                int intMax = MathHelperCalculator.MaxValue(number1, number2);
+                double doubleMax = MathHelperCalculator.MaxValue(number3, number4);
+
+                double pow = MathHelperCalculator.Pow(number4, number2);
+
+                Console.WriteLine($"1: { intSum}\n2: {doubleSum}\n3: {intDifference}\n4: {doubleDifference}\n5: {intMultiplier}\n6: {doubleMultiplier}\n7: {intAbsolutify}\n8: {doubleAbsolutify}\n9: {intMin}\n10: {doubleMin}\n11: {intMax}\n12: {doubleMax}\nBONUS: {pow}");
+            }
         }
     }
 }
