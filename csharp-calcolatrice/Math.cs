97af72b7dae09bf411ed1718df9c616f6c106f4b
FerrariMarc0 <marco1010@live.it>
2023-09-12 14:43:14 +0200

    Aggiungere i file di progetto.

---
+++ csharp-calcolatrice/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    public class Math
+    {
+        public static class MathHelperCalculator
+        {
+            public static int Sum(int a, int b)
+            {
+                return a + b;
+            }
+
+        }
+    }
+}
